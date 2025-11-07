9f0fb0f45506acbee188852e7b5b448a7da44acb
Jørgen S. Dokken <dokken@simula.no>
2025-11-07 12:25:30 +0000

    Make proper test

--- tests/test_make_tree.py
+++ tests/test_make_tree.py
@@ -5,9 +5,10 @@ from networks_fenicsx import Config, NetworkMesh, network_generation
 
 @pytest.mark.parametrize("gdim", [2, 3])
 @pytest.mark.parametrize("lcar", [1.0, 0.5, 0.1])
-def test_Y_bifurcation(gdim: int, lcar: float):
-    # Create Y bifurcation graph
-    G = network_generation.make_tree(n=2, H=1, W=1, dim=gdim)
+@pytest.mark.parametrize("n", [2, 5, 7])
+@pytest.mark.parametrize("H", [1, 2])
+def test_make_tree(n: int, H: int, gdim: int, lcar: float):
+    G = network_generation.make_tree(n=n, H=H, W=1, dim=gdim)
     config = Config()
     config.lcar = lcar
     network_mesh = NetworkMesh(G, config)
@@ -18,6 +19,12 @@ def test_Y_bifurcation(gdim: int, lcar: float):
     assert tdim == 1
     assert domain.geometry.dim == gdim
     num_cells_global = domain.topology.index_map(tdim).size_global
-    assert num_cells_global == 3 * 1 / config.lcar
+
+    num_elements_per_segment = max((H / n), 1) / lcar
+    num_segments = sum(2**i for i in range(n))
+    assert num_cells_global == num_elements_per_segment * num_segments
     num_vertices_global = domain.topology.index_map(0).size_global
-    assert num_vertices_global == 3 * 1 / config.lcar + 1
+    assert (
+        num_vertices_global
+        == num_elements_per_segment + 1 + (num_segments - 1) * num_elements_per_segment
+    )
